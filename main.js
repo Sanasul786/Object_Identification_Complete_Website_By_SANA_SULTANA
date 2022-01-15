f67938fa1a52025c6b3487540fb46d20a412eae4
Sanasul786 <71544603+Sanasul786@users.noreply.github.com>
2022-01-15 17:41:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function dogcat_page(){
+    window.location = "dogcat.html";
+}
+function bedroom_page(){
+    window.location = "bedroom.html";
+}
+function car_page(){
+    window.location = "car.html";
+}
+function kids_page(){
+    window.location = "kids.html";
+}
+function dine_page(){
+    window.location = "dine.html";
+}
+function living_page(){
+    window.location = "living.html";
+}
+function couch_page(){
+    window.location = "couch.html";
+}
